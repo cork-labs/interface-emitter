a2495a82d66495253f85b0f74190525752d0917e
Andre Torgal <andre.torgal@gmail.com>
2019-01-06 01:44:32 +0100

    fix: emit() not trigger()

--- test/index.spec.js
+++ test/index.spec.js
@@ -6,7 +6,7 @@ const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 chai.use(sinonChai);
 
-const emitterMixin = require('./emitterMixin');
+const emitterMixin = require('../src/index');
 
 describe('emitterMixin', function () {
   it('should be a function', function () {
@@ -16,7 +16,7 @@ describe('emitterMixin', function () {
   describe('api', function () {
     beforeEach(function () {
       this.object = {};
-      this._emitter = emitterMixin(this._object);
+      this.emitter = emitterMixin(this.object);
     });
 
     it('should expose the methods', function () {
@@ -25,16 +25,73 @@ describe('emitterMixin', function () {
       expect(this.object.off).to.be.a('function');
     });
 
-    describe('when emit() is invoked', function () {
+    describe('given a "on" subscriber', function () {
       beforeEach(function () {
         this.spy = sinon.spy();
-        this._emitter = emitterMixin(this._object);
         this.object.on('foobar', this.spy);
-        this._emitter.trigger('foobar', 1, 2, 3);
       });
 
-      it('should invoke the callback', function () {
-        expect(this.spy).to.have.been.calledWithExactly(1, 2, 3);
+      describe('when emit() is invoked', function () {
+        beforeEach(function () {
+          this.emitter.emit('foobar', 1, 2, 3);
+        });
+
+        it('should invoke the callback', function () {
+          expect(this.spy).to.have.been.calledWithExactly(1, 2, 3);
+        });
+
+        describe('and when emit() is invoked again', function () {
+          beforeEach(function () {
+            this.emitter.emit('foobar', 'baz');
+          });
+
+          it('should invoke the callback again', function () {
+            expect(this.spy.callCount).to.equal(2);
+            expect(this.spy).to.have.been.calledWithExactly('baz');
+          });
+        });
+      });
+
+      describe('and that subcriber unsubscribes', function () {
+        beforeEach(function () {
+          this.object.off('foobar', this.spy);
+        });
+        describe('and when emit() is invoked again', function () {
+          beforeEach(function () {
+            this.emitter.emit('foobar', 'baz');
+          });
+
+          it('should not invoke the callback', function () {
+            expect(this.spy.callCount).to.equal(0);
+          });
+        });
+      });
+    });
+
+    describe('given a "once" subscriber', function () {
+      beforeEach(function () {
+        this.spy = sinon.spy();
+        this.object.once('foobar', this.spy);
+      });
+
+      describe('when emit() is invoked', function () {
+        beforeEach(function () {
+          this.emitter.emit('foobar', 1, 2, 3);
+        });
+
+        it('should invoke the callback', function () {
+          expect(this.spy).to.have.been.calledWithExactly(1, 2, 3);
+        });
+
+        describe('and when emit() is invoked again', function () {
+          beforeEach(function () {
+            this.emitter.emit('foobar', 'baz');
+          });
+
+          it('should not invoke the callback again', function () {
+            expect(this.spy.callCount).to.equal(1);
+          });
+        });
       });
     });
   });
